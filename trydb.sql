7fe7f11ec89db1ddb4ed66525f96d6747a8f4eb3
vika <134867930+vkgithubb@users.noreply.github.com>
2024-05-23 21:08:59 +0700

    Add files via upload

---
+++ trydb.sql
@@ -0,0 +1,10 @@
+CREATE TABLE daftarBuku
+(	
+	id CHAR(50) PRIMARY KEY, 
+    judul VARCHAR(50), 
+    penulis VARCHAR(50), 
+    penerbit VARCHAR(50), 
+    tahunTerbit DATE, 
+    kategori VARCHAR(50)
+);
+
